adb42a00bdec4a28dd55cec0227a8ca6b5f7f96d
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-02-24 17:25:33 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/vite-homeworks/'
-})
+  base: "/vite-homeworks/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
