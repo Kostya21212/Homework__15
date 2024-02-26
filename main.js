721a6b099a925427bbfbaaae5af569aa9574f551
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-02-26 11:55:49 +0200

    changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import {array} from './homeworks/homework1.js'
-import { UserBalance} from './homeworks/homework3.js'
-import {positiveElements} from './homeworks/homework2.js'
+import {array} from './homeworks/homework15.js'
+import {positiveElements} from './homeworks/homework16.js'
+import { UserBalance} from './homeworks/homework17.js'
 
 const massive =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
